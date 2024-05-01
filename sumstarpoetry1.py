defd3757c307357d506ec4eb54ea136096e31259
sumytayeasmin88 <142500648+sumytayeasmin88@users.noreply.github.com>
2024-05-01 21:03:43 +1000

    Add files via upload

---
+++ sumstarpoetry1.py
@@ -0,0 +1,14 @@
+print("stars of one by sum, a poem")
+#tittle of the poem made
+import time 
+time.sleep(4) 
+
+print("Does one drown amongst the stars\nThe stars known as the guardian angels guide the pathway of the unknown\nThe stars, one lifeless soul may not known\nYet the lifeless souls are filled in the sky by the graveyard of stars crying for the naive.")
+time.sleep(5)
+print("Yearning for peace amid chaos and destruction")
+time.sleep(5)
+print("But they disappear as too much is to bear, shining light when necessary\nA remembrance that lifeless souls once lived in the chaos before seeking peace.")
+
+
+
+    

c49c2ee2d33369319c83baf4a559c37c3369b973
sumytayeasmin88 <142500648+sumytayeasmin88@users.noreply.github.com>
2024-05-07 11:43:11 +1000

    Add files via upload

---
+++ starsumpoem(4).py
@@ -0,0 +1,8 @@
+import time
+time.sleep(3) 
+print('interlink poem by sum')
+#the title for this star poem
+time.sleep(2.5)
+print("Interlink gaze up above\nWith transcedent hope and blissful sentiment.")
+time.sleep(2.5)
+print("Hand intwined crushing in desperation\nIn chances the interlink souls will be in\nOne through the dark light.")
